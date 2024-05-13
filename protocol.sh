d25e58f24de8b131ed4a2f9b81846507cf15ff89
Hristo Kosev <smanuel@healee.com>
2024-05-13 15:42:28 +0300

    Add support for deployment to production environment

--- protocol.sh
+++ protocol.sh
@@ -2,15 +2,28 @@
 
 mode=$1
 protocol=$2
+environment=$3
+
+auth="Authorization: Basic $HEALEE_STAGE_AUTH"
+url=$HEALEE_STAGE_URL
+
+if [ "$environment" = "prod" ]; then
+	auth="Authorization: Bearer $HEALEE_PROD_AUTH"
+	url=$HEALEE_PROD_URL
+fi
 
 if [ "$mode" = "fetch" ]; then
 	aws s3 sync --exact-timestamps --profile $protocol --delete s3://protocol-$protocol ./development/$protocol
 elif [ "$mode" = "notify" ]; then
-	curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/notifyProtocol/$protocol
+	if [ "$environment" = "prod" ]; then
+		echo "You can't notify a protocol in production environment."
+	else
+		curl -w "\n" -H $auth -X POST $url/api/v1/notifyProtocol/$protocol
+	fi
 elif [ "$mode" = "build" ]; then
 	if [ -f "./development/$protocol/info.json" ]; then
 		aws s3 sync --exact-timestamps --profile $protocol --delete --exclude ".git/*" --exclude "node_modules/*" --exclude "package-lock.json" ./development/$protocol s3://protocol-$protocol
-		curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/buildProtocol/$protocol
+		curl -w "\n" -H $auth -X POST $url/api/v1/buildProtocol/$protocol
 	else
 		echo "Protocol doesn't exist. Please, call fetch to retrieve the initial version of the protocol."
 	fi
